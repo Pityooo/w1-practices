06be638b5102e04be5b3dd822c29d7f59dc6e4bd
Pityooo <pityooone@gmail.com>
2022-01-20 15:36:03 +0100

    newsave

---
+++ frontend/script.js
@@ -0,0 +1,22 @@
+async function loadEvent() {
+    console.log("Page loaded :)")
+
+    const root = document.querySelector("#root")
+    
+    const countryRes = await fetch("https://restcountries.com/v3.1/all");
+    const countryArr = await countryRes.json();
+
+    // console.log(countryArr[0].name.common)
+
+    for (const country of countryArr) {
+        // console.log(country.name.common);
+
+        root.insertAdjacentHTML("beforeend", `
+            <section>
+                <h1>${country.name.common}</h1>
+                <p>${country.region}</p>
+            </section>
+        `)
+    }
+}
+window.addEventListener("load", loadEvent)
